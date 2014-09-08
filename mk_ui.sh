e71d4b13ed10b9f02d7b8355bdb262e36297e499
christop <christoph@lafkon.net>
2014-09-08 21:07:06 +0200

    use configuration from README

--- mk_ui.sh
+++ mk_ui.sh
@@ -6,7 +6,6 @@
 
   WWWDIR=~/tmp/fontain
 
-
 # TEMPLATES
 # ----------------------------------------------------------------- #
   TMPLT_CSS=lib/ui/templates/css.template
@@ -16,14 +15,235 @@
   TMPLT_AKKRDNSLIDER=lib/ui/templates/akkordeon_slider.template
   TMPLT_DOWNLOAD=lib/ui/templates/download.template
 
+# LICENSE/READMENAMES (MD OR TXT?)
+# ----------------------------------------------------------------- #
+  READMENAME=README.md
+  LICENSENAME=LICENSE.txt
+
 # COPY STATIC STUFF 
 # ----------------------------------------------------------------- #
   cp -r `ls -d lib/ui/* | egrep -v "parts"` $WWWDIR
 
-# LICENSE/READMENAMES (MD OR TXT?)
-# ----------------------------------------------------------------- #
-  READMENAME=README.txt
-  LICENSENAME=LICENSE.txt
+
+# --------------------------------------------------------------------------- #
+# FUNCTIONS
+# --------------------------------------------------------------------------- #
+
+  function AKKORDEON(){
+
+
+    if [ -f $README ]; then
+    FONTSTYLES=`sed '/^\s*$/d' $README |     # REMOVE EMPTY LINES
+                sed -e :a \
+                -e '$!N;s/\n=====/=====/;ta' \
+                -e 'P;D' |                   # APPEND LINES WITH =====
+                sed '/=====$/{x;p;x;}' |     # INSERT EMPTY LINE ABOVE
+                sed -e '/./{H;$!d;}' \
+                -e 'x;/FONT STYLES/!d;' |    # SELECT PARAGRAPH CONTAINING FONT S...
+                sed 's/^-//g' |              # REMOVE LEADING -
+                grep -v "FONT STYLES"`       # RM LINE CONTAINING FONT S...
+    else
+  
+    FONTSTYLES=`find $FONTFAMILY/src -name "*.sfdir" | \
+                rev | cut -d "/" -f 1 | rev | \
+                sed 's/.sfdir//g'` 
+
+    fi
+    if [ `echo $FONTSTYLES | wc -c` -lt 2 ]; then
+
+    FONTSTYLES=`find $FONTFAMILY/src -name "*.sfdir" | \
+                rev | cut -d "/" -f 1 | rev | \
+                sed 's/.sfdir//g'` 
+    fi
+
+
+   DISPLAY=`echo $FONTSTYLES | sed 's/ /XbdZ53cft/g'`
+   EXCLUDE=`echo $DISPLAY | sed 's/XbdZ53cft/|/g'`
+   REST=`find $FONTFAMILY/src -name "*.sfdir" | \
+         egrep -v "$EXCLUDE" | \
+         rev | cut -d "/" -f 1 | rev | \
+         sed 's/.sfdir//g' | \
+         sed 's/ /XbdZ53cft/g'`
+   HIDE=""
+
+
+
+   COUNT=100
+   for SELECTION in $DISPLAY $REST
+    do
+
+      FONTSTYLES=`echo $SELECTION | sed 's/XbdZ53cft/ /g'`
+
+      for FONTSTYLESRC in $FONTSTYLES
+       do
+
+        if [ `find $FONTFAMILY/src -name "$FONTSTYLESRC.sfdir" | wc -c` \
+             -gt 1 ]; then
+        FONTSTYLESRC=`find $FONTFAMILY/src -name "$FONTSTYLESRC.sfdir"`
+        STYLENAME=`grep -h "FullName" $FONTSTYLESRC/font.props | \
+                   cut -d ":" -f 2 | sed "s/^[ \t]*//"`
+        STYLENAMEWWW=`echo $STYLENAME | \
+                      sed 's/ /jfdDw24e/g' | \
+                      sed 's/[^a-zA-Z0-9 ]//g' | \
+                      sed 's/jfdDw24e/-/g' | \
+                      tr [:upper:] [:lower:]`
+
+        FONTSTYLESRCNAME=`basename $FONTSTYLESRC | sed "s/.sfdir//g"`
+
+          echo $STYLENAME
+        # echo $STYLENAMEWWW
+        # echo $FONTSTYLESRCNAME
+        # echo
+
+        EOTFILE=`find $FONTFAMILY -name "${FONTSTYLESRCNAME}.eot"`
+        cp -p $EOTFILE $WEBFONTTARGET
+        EOTFILE=`basename $EOTFILE`
+
+        WOFFFILE=`find $FONTFAMILY -name "${FONTSTYLESRCNAME}.woff"`
+        cp -p $WOFFFILE $WEBFONTTARGET
+        WOFFFILE=`basename $WOFFFILE`
+
+        SVGFILE=`find $FONTFAMILY -name "${FONTSTYLESRCNAME}.svg"`
+        cp -p $SVGFILE $WEBFONTTARGET
+        SVGFILE=`basename $SVGFILE`
+
+        TTFFILE=`find $FONTFAMILY -name "${FONTSTYLESRCNAME}.ttf"`
+        cp -p $TTFFILE $WEBFONTTARGET
+        TTFFILE=`basename $TTFFILE`
+
+        cat $TMPLT_CSS | \
+        sed "s/EOTFILE/$EOTFILE/g" | \
+        sed "s/SVGFILE/$SVGFILE/g" | \
+        sed "s/TTFFILE/$TTFFILE/g" | \
+        sed "s/WOFFFILE/$WOFFFILE/g" | \
+        sed "s/FONTFAMILY/$STYLENAMEWWW/g" >> $CSS
+        echo                               >> $CSS
+
+        cat $TMPLT_AKKORDION | \
+        sed "s/accordion-section positiv/& $HIDE/g" | \
+        sed "s/STYLENAMEWWW/$STYLENAMEWWW/g" | \
+        sed "s/STYLENAME/$STYLENAME/g" | \
+        sed "s/-COUNT/-$COUNT/g" | \
+        sed "s/FAMILYNAME/$FAMILYNAME/g"   >> $INDEX
+
+        COUNT=`expr $COUNT + 1`
+
+        fi
+
+       done
+
+       HIDE="excluded"
+
+    done
+
+    
+    echo "<a class="fontdemo-showmore jsonly" \
+           href="">and 12 more.</a>" | tr -s ' '       >> $INDEX
+
+
+
+   # ----------------------------------------------------------- #
+     echo '</div>'                                     >> $INDEX
+     cat $TMPLT_AKKRDNSLIDER                           >> $INDEX
+   # ----------------------------------------------------------- #
+
+
+  }
+
+# --------------------------------------------------------------------------- #
+  function AUTHOR(){
+
+    if [ -f $README ]; then
+    AUTHOR=`sed '/^\s*$/d' $README |        # REMOVE EMPTY LINES
+            sed -e :a \
+                -e '$!N;s/\n=====/=====/;ta' \
+                -e 'P;D' |                  # APPEND LINES WITH =====
+            sed '/=====$/{x;p;x;}' |        # INSERT EMPTY LINE ABOVE
+            sed -e '/./{H;$!d;}' \
+                -e 'x;/AUTHOR.*==/!d;' |    # SELECT PARAGRAPH CONTAINING AUTHOR
+            grep -v "AUTHOR"`               # RM LINE CONTAINING AUTHOR
+    else
+  
+    AUTHOR=""
+  
+    fi
+    if [ `echo $AUTHOR | wc -c` -gt 2 ]; then
+
+    AUTHOR=`echo $AUTHOR | \
+            pandoc -r markdown -w html | \
+            sed 's/<\/*p>//g'`
+
+    echo '<hr class="hrsection" />'                  >> $INDEX
+    echo '<div class="fourteen columns">'            >> $INDEX
+    echo $AUTHOR  >> $INDEX
+    echo '</div>'                                    >> $INDEX
+    echo '<br class='clear' />'                      >> $INDEX
+    echo '<hr class="hrsection" />'                  >> $INDEX
+    echo '<br class='clear' />'                      >> $INDEX
+
+    fi
+
+  }
+
+# --------------------------------------------------------------------------- #
+  function LICENSE(){
+
+    if [ -f $README ]; then
+    LICENSE=`sed '/^\s*$/d' $README |     # REMOVE EMPTY LINES
+             sed -e :a \
+                 -e '$!N;s/\n=====/=====/;ta' \
+                 -e 'P;D' |               # APPEND LINES WITH =====
+             sed '/=====$/{x;p;x;}' |     # INSERT EMPTY LINE ABOVE
+             sed -e '/./{H;$!d;}' \
+                 -e 'x;/LICENSE==/!d;' |  # SELECT PARAGRAPH CONTAINING OR
+             grep -v "LICENSE=="`         # RM LINE CONTAINING AUTHOR
+    else
+  
+    LICENSE="no license provided"
+  
+    fi
+    if [ `echo $LICENSE | wc -c` -lt 2 ]; then
+
+    LICENSE="no author provided"
+
+    fi
+
+    LICENSE=`echo $LICENSE | \
+             pandoc -r markdown -w html | \
+             sed 's/<\/*p>//g'`
+
+    echo $LICENSE
+
+  }
+
+# --------------------------------------------------------------------------- #
+  function SPECIMEN(){
+
+    echo "now writing specimen"
+
+  }
+
+# --------------------------------------------------------------------------- #
+  function DOWNLOAD(){
+
+    echo "now writing download"
+
+  }
+
+# --------------------------------------------------------------------------- #
+  function FLOWTEXT(){
+
+    echo "now writing flowtext"
+
+  }
+# --------------------------------------------------------------------------- #
+
+
+
+
+
+
+
 
 
 
@@ -31,14 +251,13 @@
 # CREATE WWW USER INTERFACE
 # ================================================================= #
 
-  for FONTFAMILY in $FONTS
-   do
+ for FONTFAMILY in $FONTS
+  do
       FAMILYNAME=`grep -h "FamilyName" $FONTFAMILY/src/*/font.props | \
-                   cut -d ":" -f 2 | \
-                   awk '{print length, $0;}' | sort -n | \
-                   head -n 1 | cut -d " " -f 2- | \
-                   sed "s/^[ \t]*//"`
-    # FAMILYNAMEWWW=`echo $FAMILYNAME | sed 's/ //g'`
+                  cut -d ":" -f 2 | \
+                  awk '{print length, $0;}' | sort -n | \
+                  head -n 1 | cut -d " " -f 2- | \
+                  sed "s/^[ \t]*//"`
       FAMILYNAMEWWW=`echo $FAMILYNAME | \
                      sed 's/ /jfdDw24e/g' | \
                      sed 's/[^a-zA-Z0-9 ]//g' | \
@@ -55,244 +274,108 @@
       CSS=$WEBFONTTARGET/webfont.css
       INDEX=$FAMILYTARGET/index.html
 
-
       mkdir -p $FAMILYTARGET
       mkdir -p $WEBFONTTARGET
       mkdir -p $SPECIMENTARGET
       mkdir -p $EXPORTTARGET
       if [ -f $CSS ]; then rm $CSS ; fi
 
+      README=$FONTFAMILY/$READMENAME
 
-   # =========================================================== #
-   # CREATE HTML FILE
-   # =========================================================== #
-     cat $TMPLT_HEAD                                   >  $INDEX
+# --------------------------------------------------------------------------- #
+# GET UI CONFIGURATION FROM README
+# --------------------------------------------------------------------------- #
+  if [ -f $README ]; then
+  SECTIONS=`sed '/^\s*$/d' $README |        # REMOVE EMPTY LINES
+            sed -e :a \
+            -e '$!N;s/\n=====/=====/;ta' \
+            -e 'P;D' |                      # APPEND LINES WITH =====
+            sed '/=====$/{x;p;x;}' |        # INSERT EMPTY LINE ABOVE
+            sed -e '/./{H;$!d;}' \
+            -e 'x;/UI CONFIGURATION/!d;' |  # SELECT PARAGRAPH CONTAINING UI C..
+            grep -v "UI CONFIGURATION"`     # RM LINE CONTAINING UI C..
+  else
 
-   # ----------------------------------------------------------- #
-     echo '<div class="sixteen columns accordion" \
-            id="sortable">' | tr -s ' '                >> $INDEX
+  SECTIONS="AKKORDEON DOWNLOAD AUTHOR SPECIMEN LICENSE"
 
-   # USE CUSTOM ORDER IF AVAILABLE
-   # ----------------------------------------------------------- #
-     if [ -f $FONTFAMILY/specimen/selection.list ]; then
-
-         SELECTION=
-         for FONTSTYLE in `cat $FONTFAMILY/specimen/selection.list`
-          do
-             FONTSTYLE=`basename $FONTSTYLE | cut -d "." -f 1`
-             SELECTION="$SELECTION "`find $FONTFAMILY/src/ \
-                                          -name "${FONTSTYLE}.sfdir"`
-         done
-         SELECTION=`echo $SELECTION | # 
-                    sed 's/ /\n /g' | # SPACES TO LINEBREAK 
-                    awk ' !x[$0]++'`  # REMOVE IDENTICAL LINES
-
-     else
-         SELECTION=`ls -d -1 $FONTFAMILY/src/*.sfdir`
-     fi
-
-   # ----------------------------------------------------------- #
-   # AKKORDEON LOOP FOR FONTSTYLES
-   # ----------------------------------------------------------- #
-     COUNT=1
-     for FONTSTYLE in $SELECTION
-      do
-          STYLENAME=`grep -h "FullName" $FONTSTYLE/font.props | \
-                      cut -d ":" -f 2 | sed "s/^[ \t]*//"`
-        # STYLENAMEWWW=`echo $STYLENAME | sed 's/ /_/g'`
-          STYLENAMEWWW=`echo $STYLENAME | \
-                        sed 's/ /jfdDw24e/g' | \
-                        sed 's/[^a-zA-Z0-9 ]//g' | \
-                        sed 's/jfdDw24e/-/g' | \
-                        tr [:upper:] [:lower:]`
-
-          FONTSTYLESRC=`basename $FONTSTYLE | sed "s/.sfdir//g"` 
-
-
-          EOTFILE=`find $FONTFAMILY -name "${FONTSTYLESRC}.eot"`
-          cp -p $EOTFILE $WEBFONTTARGET
-          EOTFILE=`basename $EOTFILE`
-
-          WOFFFILE=`find $FONTFAMILY -name "${FONTSTYLESRC}.woff"`
-          cp -p $WOFFFILE $WEBFONTTARGET
-          WOFFFILE=`basename $WOFFFILE`
-
-          SVGFILE=`find $FONTFAMILY -name "${FONTSTYLESRC}.svg"`
-          cp -p $SVGFILE $WEBFONTTARGET
-          SVGFILE=`basename $SVGFILE`
-
-          TTFFILE=`find $FONTFAMILY -name "${FONTSTYLESRC}.ttf"`
-          cp -p $TTFFILE $WEBFONTTARGET
-          TTFFILE=`basename $TTFFILE`
-
-
-          cat $TMPLT_CSS | \
-          sed "s/EOTFILE/$EOTFILE/g" | \
-          sed "s/SVGFILE/$SVGFILE/g" | \
-          sed "s/TTFFILE/$TTFFILE/g" | \
-          sed "s/WOFFFILE/$WOFFFILE/g" | \
-          sed "s/FONTFAMILY/$STYLENAMEWWW/g" >> $CSS
-          echo                               >> $CSS
-
-          cat $TMPLT_AKKORDION | \
-          sed "s/STYLENAMEWWW/$STYLENAMEWWW/g" | \
-          sed "s/STYLENAME/$STYLENAME/g" | \
-          sed "s/-COUNT/-$COUNT/g" | \
-          sed "s/FAMILYNAME/$FAMILYNAME/g"  >> $INDEX
-
-          COUNT=`expr $COUNT + 1`
-
-     done
-   # ----------------------------------------------------------- #
-     echo '</div>'                                     >> $INDEX
-     cat $TMPLT_AKKRDNSLIDER                           >> $INDEX
-   # ----------------------------------------------------------- #
-     cat $TMPLT_DOWNLOAD                               >> $INDEX
-   # ----------------------------------------------------------- #
-     if [ -f $SPECIMENSRC/info.md ]; then
-
-      echo '<div class="fourteen columns">'            >> $INDEX
-      cat $SPECIMENSRC/info.md | \
-      pandoc -r markdown -w html                       >> $INDEX
-      echo '</div>'                                    >> $INDEX
-
-     fi
-   # ----------------------------------------------------------- #
-     if [ -f $SPECIMENSRC/*.svg ]; then
-      for SVGSRC in `ls $SPECIMENSRC/*.svg`
-       do
-          SVGTARGET=$SPECIMENTARGET/`basename $SVGSRC | \
-                                     rev | \
-                                     cut -d "." -f 2- | rev`.png
-          inkscape --export-png=$SVGTARGET \
-                   $SVGSRC > /dev/null
-      done
-     fi
-   # ----------------------------------------------------------- #
-     if [ -f $SPECIMENSRC/*.png ]; then
-          cp $SPECIMENSRC/*.png $SPECIMENTARGET
-     fi
-   # ----------------------------------------------------------- #
-     if [ -f $SPECIMENTARGET/*.png ]; then
-      for PNG in `ls $SPECIMENTARGET/*.png`
-       do
-          PNG=`echo $PNG | rev | cut -d "/" -f 1-2 | rev`
-          echo "<img class=\"sixteen columns specipic\" \
-                 src=\"$PNG\" />" | tr -s ' '          >> $INDEX
-      done
-     fi
-   # ----------------------------------------------------------- #
-     cat $TMPLT_FOOT                                   >> $INDEX
-
-
-   # =========================================================== #
-   # CREATE DOWNLOADS
-   # =========================================================== #
-     ZIPNAME=$FAMILYNAMEWWW
-
-   # ----------------------------------------------------------- #
-   # CREATE DIRECTORIES
-   # ----------------------------------------------------------- #
-     for EXPORT in ttf otf ufo webfont tex 
-      do
-         mkdir -p $EXPORTTARGET/$EXPORT
-     done
-
-   # ----------------------------------------------------------- #
-   # ZIP WEBFONT DIRECTORY (IF THERE EXISTS A NEWER SOURCE)
-   # ----------------------------------------------------------- #
-     NEWESTFILE=`find $FONTFAMILY -type f -printf '%T@ %p\n' | \
-                 egrep ".ttf|.eot|.woff|.svg" | \
-                 sort -n | tail -1 | cut -f 2- -d " "`
-
-     if [ `find $EXPORTTARGET/webfont/  -name "*.zip" \
-           -newer $NEWESTFILE | wc -l` -gt 0 ]
-     then
-
-       echo "${ZIPNAME}.webfont.zip is up-to-date"
-
-     else
-
-       if [ -f $FONTFAMILY/$READMENAME ]; then
-       cp $FONTFAMILY/$READMENAME  $WEBFONTTARGET ; fi
-       if [ -f $FONTFAMILY/$LICENSENAME ]; then
-       cp $FONTFAMILY/$LICENSENAME $WEBFONTTARGET ; fi
-  
-       cd $WEBFONTTARGET
-  
-          zip -r ${ZIPNAME}.webfont.zip *.*
-  
-          if [ -f $READMENAME ];  then rm $READMENAME  ; fi
-          if [ -f $LICENSENAME ]; then rm $LICENSENAME ; fi
-
-        # touch -r `ls -tr *.* | egrep -v ".zip|.css" | \
-        #           tail -n 1` \
-        #       ${ZIPNAME}.webfont.zip
-
-       cd - > /dev/null
-
-   # MOVE ZIP TO LOCATION
-   # ----------------------------------------------------------- #
-       mv $WEBFONTTARGET/${ZIPNAME}.webfont.zip $EXPORTTARGET/webfont
-
-     fi
-
-       ZIPLINKFOO=WWWZIP
-       sed -i "s,$ZIPLINKFOO,export/webfont/${ZIPNAME}.webfont.zip,g" $INDEX
+  fi
+# --------------------------------------------------------------------------- #
+# MAKE SURE THERE IS AT LEAST AKKORDEON AND DOWNLOAD
+# --------------------------------------------------------------------------- #
+  if [ `echo $SECTIONS | grep "DOWNLOAD" | wc -l` -lt 1 ]
+  then  SECTIONS="DOWNLOAD $SECTIONS" ; fi
+  if [ `echo $SECTIONS | grep "AKKORDEON" | wc -l` -lt 1 ]
+  then  SECTIONS="AKKORDEON $SECTIONS" ; fi
 
 
 
-   # ----------------------------------------------------------- #
-   # ZIP THE REST (IF THERE EXISTS A NEWER SOURCE)
-   # ----------------------------------------------------------- #
-     for FORMAT in ttf otf ufo
-      do
-         NEWESTFILE=`find $FONTFAMILY/export/$FORMAT  \
-                          -type f -printf '%T@ %p\n' | \
-                     sort -n | tail -1 | cut -f 2- -d " "`
-
-        ZIPTARGET=${ZIPNAME}.$FORMAT.zip
-
-        if [ `find $EXPORTTARGET/$FORMAT/ -name "*.zip" \
-              -newer $NEWESTFILE | wc -l` -gt 0 ]
-        then
-
-           echo "$ZIPTARGET is up-to-date"
-
-        else
-
-           cd $FONTFAMILY/export/$FORMAT
-
-           if [ -f ../../$READMENAME ]; then
-           cp ../../$READMENAME . ; fi
-           if [ -f ../../$LICENSENAME ]; then
-           cp ../../$LICENSENAME . ; fi
-
-           zip -r X-${ZIPNAME}.$FORMAT.zip *.*
-
-           if [ -f $READMENAME ];  then rm $READMENAME  ; fi
-           if [ -f $LICENSENAME ]; then rm $LICENSENAME ; fi
-
-           cd - > /dev/null
-
-   # MOVE ZIP TO LOCATION
-   # ----------------------------------------------------------- #
-           mv $FONTFAMILY/export/$FORMAT/X-${ZIPNAME}.$FORMAT.zip \
-              $EXPORTTARGET/$FORMAT/${ZIPNAME}.$FORMAT.zip
-        fi
-
-        ZIPLINKFOO=`echo $FORMAT | tr [:lower:] [:upper:]`ZIP
-        sed -i "s,$ZIPLINKFOO,export/$FORMAT/${ZIPNAME}.$FORMAT.zip,g" $INDEX
 
 
-     done
-   # =========================================================== #
+# --------------------------------------------------------------------------- #
+# CREATE HTML FILE
+# --------------------------------------------------------------------------- #
+  cat $TMPLT_HEAD                                                   >  $INDEX
 
-     sed -i "s/FONTFAMILY/$FAMILYNAME/g"                  $INDEX
+  echo '<div class="sixteen columns accordion" id="sortable">'      >> $INDEX
 
+# --------------------------------------------------------------------------- #
+# JUST DO IT
+# --------------------------------------------------------------------------- #
+  for SACTION in $SECTIONS
+   do
+      if [ `grep "function $SACTION" $0 | wc -l` -gt 0 ]; then
+ 
+        $SACTION 
 
-
+      fi
   done
+# --------------------------------------------------------------------------- #
+  cat $TMPLT_FOOT                                                   >> $INDEX
+# --------------------------------------------------------------------------- #
+
+
+
+ done
+
+
+
+exit 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  README=README_2.md
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
