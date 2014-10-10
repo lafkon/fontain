7d89f9d0b8eaed76a874d6f6e7fd07652d771d61
christop <christoph@lafkon.net>
2014-10-10 15:20:09 +0200

    no tex conversion for now

--- mk_fonts.sh
+++ mk_fonts.sh
@@ -111,10 +111,7 @@
       done
 
 
-    # PREPARE FONT/STRUCTURE FOR USE WITH LATEX
-    # =========================================================== #
-      TEXMFROOT=$EXPORTROOT/tex
-      if [ ! -f $TEXMFROOT ]; then mkdir -p $TEXMFROOT ; fi
+
 
     # CHECK CONFIGURATION 
     # ----------------------------------------------------------- #
@@ -122,157 +119,171 @@
       KBS=`grep KARLBERRYNAME $README | head -1 | cut -d ":" -f 2`
 
 
-    # CREATE POSTSCRIPT TYPE1 SOURCES
-    # ----------------------------------------------------------- #
-      mkdir $TMP/ps1src
+      if [ `echo $KBS | wc -c` -gt 1 ]; then 
 
-      for CUT in `grep TEXSRCREGULAR      $README | head -1`:r \
-                 `grep TEXSRCITALIC       $README | head -1`:ri \
-                 `grep TEXSRCBOLD         $README | head -1`:b \
-                 `grep TEXSRCBOLD-ITALIC  $README | head -1`:bi
-       do
+      echo "tex configuration for $FONTROOT exists"
 
-       if [ `echo $CUT | cut -d ":" -f 2 | wc -c` -gt 4 ]; then
+#    # PREPARE FONT/STRUCTURE FOR USE WITH LATEX
+#    # =========================================================== #
+#      TEXMFROOT=$EXPORTROOT/tex
+#      if [ ! -f $TEXMFROOT ]; then mkdir -p $TEXMFROOT ; fi
+#
+#    # CREATE POSTSCRIPT TYPE1 SOURCES
+#    # ----------------------------------------------------------- #
+#      mkdir $TMP/ps1src
+#
+#      for CUT in `grep TEXSRCREGULAR      $README | head -1`:r \
+#                 `grep TEXSRCITALIC       $README | head -1`:ri \
+#                 `grep TEXSRCBOLD         $README | head -1`:b \
+#                 `grep TEXSRCBOLD-ITALIC  $README | head -1`:bi
+#       do
+#
+#       if [ `echo $CUT | cut -d ":" -f 2 | wc -c` -gt 4 ]; then
+#
+#        CUTSRC=`echo $CUT | cut -d ":" -f 2`
+#        KBSCUT=`echo $CUT | cut -d ":" -f 3`
+#
+#        SRC=`find $FONTROOT -name "${CUTSRC}.sfdir"`
+#        SRCROOT=`echo $SRC | rev | cut -d "/" -f 2- | rev`
+#
+#      # SAFETY CHECK
+#        if [ `echo $SRCROOT | wc -c` -gt 4 ]; then       
+#
+#        $FONTCONVERT --pstype1 $SRC
+#        mv $SRCROOT/*.pfb $TMP/ps1src/${KBS}${KBSCUT}8a.pfb
+#        mv $SRCROOT/*.afm $TMP/ps1src/${KBS}${KBSCUT}8a.afm
+#
+#        fi
+#
+#       fi
+#
+#     done
+#
+#    # SET TEXMF VARIABLES
+#    # ----------------------------------------------------------- #
+#    
+#      KBFNAME=$KBS
+#      FOUNDRYNAME=`grep FOUNDRY $README | head -1 | cut -d ":" -f 2`
+#      if [ `echo $FOUNDRYNAME | wc -c` -lt 2 ]; then
+#      FOUNDRYNAME=XXX
+#      fi
+#      LICENSE=$PWD/$FONTROOT/LICENSE.txt    
+#      README=$PWD/$FONTROOT/$READMENAME
+#      PSTYPE1DIR="$TMP/ps1src"
+#
+#      TEXMF=$TMP/TEXMF
+#      mkdir $TEXMF
+#    
+#      DRV="$KBFNAME-drv.tex"
+#      MAP="$KBFNAME-map.tex"
+#   
+#      cp ${PSTYPE1DIR}/${KBFNAME}* $TMP
+#
+#    # CREATE TEXMF STRUCTURE
+#    # ----------------------------------------------------------- #
+#
+#      cd $TMP
+#    
+#      echo "% $DRV"                              >  $DRV
+#      echo "\input fontinst.sty"                 >> $DRV
+#      echo "\recordtransforms{$KBFNAME-rec.tex}" >> $DRV
+#      echo "\latinfamily{$KBFNAME}{}"            >> $DRV
+#      echo "\endrecordtransforms"                >> $DRV
+#      echo "\bye"                                >> $DRV
+#    
+#      tex $DRV
+#    
+#      for F in *.pl;  do pltotf $F; done
+#      for F in *.vpl; do vptovf $F; done
+#        
+#      mkdir -p $TEXMF/fonts/afm/$FOUNDRYNAME/$FONTNAME
+#      mkdir -p $TEXMF/fonts/tfm/$FOUNDRYNAME/$FONTNAME
+#      mkdir -p $TEXMF/fonts/type1/$FOUNDRYNAME/$FONTNAME
+#      mkdir -p $TEXMF/fonts/vf/$FOUNDRYNAME/$FONTNAME    
+#      mkdir -p $TEXMF/tex/latex/$FOUNDRYNAME/$FONTNAME
+#    
+#      mv *.afm $TEXMF/fonts/afm/$FOUNDRYNAME/$FONTNAME/
+#      cp $LICENSE $TEXMF/fonts/afm/$FOUNDRYNAME/$FONTNAME/
+#      mv *.tfm $TEXMF/fonts/tfm/$FOUNDRYNAME/$FONTNAME/
+#      cp $LICENSE $TEXMF/fonts/tfm/$FOUNDRYNAME/$FONTNAME/
+#      mv *.vf $TEXMF/fonts/vf/$FOUNDRYNAME/$FONTNAME/
+#      cp $LICENSE $TEXMF/fonts/vf/$FOUNDRYNAME/$FONTNAME/
+#      mv *.pfb $TEXMF/fonts/type1/$FOUNDRYNAME/$FONTNAME/
+#      cp $LICENSE $TEXMF/fonts/type1/$FOUNDRYNAME/$FONTNAME/
+#      mv *.fd $TEXMF/tex/latex/$FOUNDRYNAME/$FONTNAME/
+#      cp $LICENSE $TEXMF/tex/latex/$FOUNDRYNAME/$FONTNAME/
+#    
+#      echo "\input finstmsc.sty"                 >  $MAP
+#      echo "\resetstr{PSfontsuffix}{.pfb}"       >> $MAP
+#      echo "\adddriver{dvips}{$KBFNAME.map}"     >> $MAP
+#      echo "\input $KBFNAME-rec.tex"             >> $MAP
+#      echo "\donedrivers"                        >> $MAP
+#      echo "\bye"                                >> $MAP
+#    
+#      tex $MAP
+#    
+#      mkdir -p $TEXMF/dvips/config/
+#      mkdir -p $TEXMF/fonts/map/dvips/
+#      mkdir -p $TEXMF/web2c/
+# 
+#      cp $KBFNAME.map $TEXMF/dvips/config/
+#      cp $KBFNAME.map $TEXMF/fonts/map/dvips/
+#    
+#      rm *.vpl *.pl *.tex *.mtx *.map # *.log
+#
+#      FONTTEST=example_${FONTNAME}.tex
+#
+#      echo '\documentclass{article}'                           >  $FONTTEST
+#      echo '\parindent=0pt'                                    >> $FONTTEST
+#      echo '\pagestyle{empty}'                                 >> $FONTTEST
+#      echo '\setlength{\textheight}{.85\paperheight}'          >> $FONTTEST
+#      echo '\setlength{\topmargin}{-.07\textheight}'           >> $FONTTEST
+#      echo '\newcommand{\testblock}[1]{'                       >> $FONTTEST
+#      echo "{\fontfamily{$KBFNAME}\selectfont#1%"              >> $FONTTEST
+#      echo "Lorem ipsum dolor sit amet, consectetur             
+#            adipisicing elit, sed do eiusmod tempor incididunt  
+#            ut labore et dolore magna aliqua. \textit{Ut enim    
+#            ad minim veniam, quis nostrud exercitation ullamco   
+#            laboris nisi ut aliquip ex ea commodo consequat.    
+#            Duis aute irure dolor in reprehenderit in voluptate 
+#            velit esse cillum dolore eu fugiat} nulla pariatur.  
+#            \textbf{Excepteur sint occaecat cupidatat non       
+#            proident, sunt in culpa qui officia deserunt        
+#            mollit anim id est laborum.}" | \
+#            tr -s ' ' | fold -s -w 60 | sed -e 's/^[ \t]*//'   >> $FONTTEST
+#      echo '\newline'                                          >> $FONTTEST
+#      echo 'fi ff fj ffi ffl fa fe fo fr fs ft fb fh fu fy f.' >> $FONTTEST
+#      echo ''                                                  >> $FONTTEST
+#      echo '\vfill'                                            >> $FONTTEST
+#      echo '}}'                                                >> $FONTTEST
+#      echo '\begin{document}'                                  >> $FONTTEST
+#      echo "\testblock{\tiny}"                                 >> $FONTTEST
+#      echo "\testblock{\scriptsize}"                           >> $FONTTEST
+#      echo "\testblock{\footnotesize}"                         >> $FONTTEST
+#      echo "\testblock{\small}"                                >> $FONTTEST
+#      echo "\testblock{\normalsize}"                           >> $FONTTEST
+#      echo "\testblock{\large}"                                >> $FONTTEST
+#      echo "\testblock{\Large}"                                >> $FONTTEST
+#      echo "\testblock{\LARGE}"                                >> $FONTTEST
+#      echo "\testblock{\huge}"                                 >> $FONTTEST
+#      echo "\testblock{\Huge}"                                 >> $FONTTEST
+#      echo '\end{document}'                                    >> $FONTTEST
+#
+#    # zip -r ${FONTNAME}.texmf.zip TEXMF README.txt $FONTTEST
+#      zip -r ${FONTNAME}.texmf.zip TEXMF
+#
+#      cd -
+#
+#      mv $TMP/${FONTNAME}.texmf.zip $TEXMFROOT
+#      mv $TMP/$FONTTEST             $TEXMFROOT
 
-        CUTSRC=`echo $CUT | cut -d ":" -f 2`
-        KBSCUT=`echo $CUT | cut -d ":" -f 3`
+       else
 
-        SRC=`find $FONTROOT -name "${CUTSRC}.sfdir"`
-        SRCROOT=`echo $SRC | rev | cut -d "/" -f 2- | rev`
-
-      # SAFETY CHECK
-        if [ `echo $SRCROOT | wc -c` -gt 4 ]; then       
-
-        $FONTCONVERT --pstype1 $SRC
-        mv $SRCROOT/*.pfb $TMP/ps1src/${KBS}${KBSCUT}8a.pfb
-        mv $SRCROOT/*.afm $TMP/ps1src/${KBS}${KBSCUT}8a.afm
-
-        fi
-
+       echo "make no tex files for $FONTROOT"
+ 
        fi
 
-     done
-
-    # SET TEXMF VARIABLES
-    # ----------------------------------------------------------- #
-    
-      KBFNAME=$KBS
-      FOUNDRYNAME=`grep FOUNDRY $README | head -1 | cut -d ":" -f 2`
-      if [ `echo $FOUNDRYNAME | wc -c` -lt 2 ]; then
-      FOUNDRYNAME=XXX
-      fi
-      LICENSE=$PWD/$FONTROOT/LICENSE.txt    
-      README=$PWD/$FONTROOT/$READMENAME
-      PSTYPE1DIR="$TMP/ps1src"
-
-      TEXMF=$TMP/TEXMF
-      mkdir $TEXMF
-    
-      DRV="$KBFNAME-drv.tex"
-      MAP="$KBFNAME-map.tex"
-   
-      cp ${PSTYPE1DIR}/${KBFNAME}* $TMP
-
-    # CREATE TEXMF STRUCTURE
-    # ----------------------------------------------------------- #
-
-      cd $TMP
-    
-      echo "% $DRV"                              >  $DRV
-      echo "\input fontinst.sty"                 >> $DRV
-      echo "\recordtransforms{$KBFNAME-rec.tex}" >> $DRV
-      echo "\latinfamily{$KBFNAME}{}"            >> $DRV
-      echo "\endrecordtransforms"                >> $DRV
-      echo "\bye"                                >> $DRV
-    
-      tex $DRV
-    
-      for F in *.pl;  do pltotf $F; done
-      for F in *.vpl; do vptovf $F; done
-        
-      mkdir -p $TEXMF/fonts/afm/$FOUNDRYNAME/$FONTNAME
-      mkdir -p $TEXMF/fonts/tfm/$FOUNDRYNAME/$FONTNAME
-      mkdir -p $TEXMF/fonts/type1/$FOUNDRYNAME/$FONTNAME
-      mkdir -p $TEXMF/fonts/vf/$FOUNDRYNAME/$FONTNAME    
-      mkdir -p $TEXMF/tex/latex/$FOUNDRYNAME/$FONTNAME
-    
-      mv *.afm $TEXMF/fonts/afm/$FOUNDRYNAME/$FONTNAME/
-      cp $LICENSE $TEXMF/fonts/afm/$FOUNDRYNAME/$FONTNAME/
-      mv *.tfm $TEXMF/fonts/tfm/$FOUNDRYNAME/$FONTNAME/
-      cp $LICENSE $TEXMF/fonts/tfm/$FOUNDRYNAME/$FONTNAME/
-      mv *.vf $TEXMF/fonts/vf/$FOUNDRYNAME/$FONTNAME/
-      cp $LICENSE $TEXMF/fonts/vf/$FOUNDRYNAME/$FONTNAME/
-      mv *.pfb $TEXMF/fonts/type1/$FOUNDRYNAME/$FONTNAME/
-      cp $LICENSE $TEXMF/fonts/type1/$FOUNDRYNAME/$FONTNAME/
-      mv *.fd $TEXMF/tex/latex/$FOUNDRYNAME/$FONTNAME/
-      cp $LICENSE $TEXMF/tex/latex/$FOUNDRYNAME/$FONTNAME/
-    
-      echo "\input finstmsc.sty"                 >  $MAP
-      echo "\resetstr{PSfontsuffix}{.pfb}"       >> $MAP
-      echo "\adddriver{dvips}{$KBFNAME.map}"     >> $MAP
-      echo "\input $KBFNAME-rec.tex"             >> $MAP
-      echo "\donedrivers"                        >> $MAP
-      echo "\bye"                                >> $MAP
-    
-      tex $MAP
-    
-      mkdir -p $TEXMF/dvips/config/
-      mkdir -p $TEXMF/fonts/map/dvips/
-      mkdir -p $TEXMF/web2c/
- 
-      cp $KBFNAME.map $TEXMF/dvips/config/
-      cp $KBFNAME.map $TEXMF/fonts/map/dvips/
-    
-      rm *.vpl *.pl *.tex *.mtx *.map # *.log
-
-      FONTTEST=example_${FONTNAME}.tex
-
-      echo '\documentclass{article}'                           >  $FONTTEST
-      echo '\parindent=0pt'                                    >> $FONTTEST
-      echo '\pagestyle{empty}'                                 >> $FONTTEST
-      echo '\setlength{\textheight}{.85\paperheight}'          >> $FONTTEST
-      echo '\setlength{\topmargin}{-.07\textheight}'           >> $FONTTEST
-      echo '\newcommand{\testblock}[1]{'                       >> $FONTTEST
-      echo "{\fontfamily{$KBFNAME}\selectfont#1%"              >> $FONTTEST
-      echo "Lorem ipsum dolor sit amet, consectetur             
-            adipisicing elit, sed do eiusmod tempor incididunt  
-            ut labore et dolore magna aliqua. \textit{Ut enim    
-            ad minim veniam, quis nostrud exercitation ullamco   
-            laboris nisi ut aliquip ex ea commodo consequat.    
-            Duis aute irure dolor in reprehenderit in voluptate 
-            velit esse cillum dolore eu fugiat} nulla pariatur.  
-            \textbf{Excepteur sint occaecat cupidatat non       
-            proident, sunt in culpa qui officia deserunt        
-            mollit anim id est laborum.}" | \
-            tr -s ' ' | fold -s -w 60 | sed -e 's/^[ \t]*//'   >> $FONTTEST
-      echo '\newline'                                          >> $FONTTEST
-      echo 'fi ff fj ffi ffl fa fe fo fr fs ft fb fh fu fy f.' >> $FONTTEST
-      echo ''                                                  >> $FONTTEST
-      echo '\vfill'                                            >> $FONTTEST
-      echo '}}'                                                >> $FONTTEST
-      echo '\begin{document}'                                  >> $FONTTEST
-      echo "\testblock{\tiny}"                                 >> $FONTTEST
-      echo "\testblock{\scriptsize}"                           >> $FONTTEST
-      echo "\testblock{\footnotesize}"                         >> $FONTTEST
-      echo "\testblock{\small}"                                >> $FONTTEST
-      echo "\testblock{\normalsize}"                           >> $FONTTEST
-      echo "\testblock{\large}"                                >> $FONTTEST
-      echo "\testblock{\Large}"                                >> $FONTTEST
-      echo "\testblock{\LARGE}"                                >> $FONTTEST
-      echo "\testblock{\huge}"                                 >> $FONTTEST
-      echo "\testblock{\Huge}"                                 >> $FONTTEST
-      echo '\end{document}'                                    >> $FONTTEST
-
-    # zip -r ${FONTNAME}.texmf.zip TEXMF README.txt $FONTTEST
-      zip -r ${FONTNAME}.texmf.zip TEXMF
-
-
-      cd -
-
-      mv $TMP/${FONTNAME}.texmf.zip $TEXMFROOT
-      mv $TMP/$FONTTEST             $TEXMFROOT
-
-      rm -r $TMP
+       if [ -d $TMP ]; then rm -r $TMP ; fi
 
   done
 
